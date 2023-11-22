80e086cc39910bb6d8def210246d0c35368fd8da
ks31030 <143372209+ks31030@users.noreply.github.com>
2023-11-22 14:50:55 +0900

    ff

---
+++ spring-DB-Oracle-1/src/main/java/com/kh/springdb/run/SpringDbOracle1Application.java
@@ -0,0 +1,13 @@
+package com.kh.springdb.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbOracle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbOracle1Application.class, args);
+	}
+
+}
