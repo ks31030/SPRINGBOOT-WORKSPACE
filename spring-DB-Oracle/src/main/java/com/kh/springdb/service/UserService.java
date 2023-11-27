1fc0087db70cf40df4db5528d5c51f3837aefc30
ks31030 <143372209+ks31030@users.noreply.github.com>
2023-11-27 10:18:19 +0900

    ff

--- spring-DB-Oracle/src/main/java/com/kh/springdb/service/UserService.java
+++ spring-DB-Oracle/src/main/java/com/kh/springdb/service/UserService.java
@@ -1,4 +1,5 @@
 package com.kh.springdb.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,19 +7,30 @@ import org.springframework.stereotype.Service;
 
 import com.kh.springdb.mapper.UserMapper;
 import com.kh.springdb.model.User;
+
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserMapper userMapper;
 	
-	//전체 회원 정보 가져오기
+	//전체 회원 조회 가져오기
 	public List<User> getAllUsers(){
 		return userMapper.getAllUsers();
 	}
 	
-	//한개 회원 정보 가져 오기
-	public User getUserById(int id) {
-		return userMapper.getUserById(id);
+	//하나의 회원 정보 가져오기
+	public User getUserById(int mno) {
+		return userMapper.getUserById(mno);
+	}
+	
+	//한명의 회원 정보 DB에 저장하기
+	public void registerUser(User user) {
+		userMapper.insertUser(user);
+	}
+	
+	//회원의 정보 수정해서 DB에 저장하기
+	public void updateUser(User user) {
+		userMapper.updateUser(user);
 	}
 }
