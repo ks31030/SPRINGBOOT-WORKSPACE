3ed185d09bbbecebbee4817a336a147c5a7b1387
ks31030 <143372209+ks31030@users.noreply.github.com>
2023-12-13 17:19:27 +0900

    ff

--- Final-JPA-Shop/src/main/java/com/kh/springdb/model/Product.java
+++ Final-JPA-Shop/src/main/java/com/kh/springdb/model/Product.java
@@ -1,6 +1,7 @@
 package com.kh.springdb.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -17,8 +18,9 @@ import lombok.*;
 public class Product {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator="produc_seq")
-	@SequenceGenerator(name = "produc_seq", sequenceName="produc_seq", allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator="pros_seq")
+	@SequenceGenerator(name = "pros_seq", sequenceName="pros_seq", allocationSize=1)
+	//private int productId;
 	private int id;
 	
 	private String name;
@@ -33,6 +35,13 @@ public class Product {
 	
 	private int isSoldout;
 	
+	
+	//댓글 작성을 위한 Comment
+	//@OneToMany(mappedBy="products", cascade=CascadeType.ALL)
+	//댓글 작성을 위한 Comment
+	@OneToMany(mappedBy="product", cascade=CascadeType.ALL)
+	private List<Comment> comments;
+	
 	//상품 이미지를 위한 필드 설정
 	
 	private String imgName;
@@ -41,7 +50,18 @@ public class Product {
 	@DateTimeFormat(pattern = "yyyy-mm-dd")
 	private LocalDate createDate;
 	
-	public void createDate() {
-		this.createDate = LocalDate.now();
-	}
+    @PrePersist // DB에 INSERT 되기 직전에 실행. 즉 DB에 값을 넣으면 자동으로 실행됨
+    public void createDate() {
+        this.createDate = LocalDate.now();
+    }
+	
+    //상품 좋아요 클릭해서 횟수 추가하기
+    private int likes;//좋아요를 받는 방법은 여러 방법이 있음.
+    //1. 사용자 관계없이 카운트만 올라가게 하기
+    
+    //2. ManyToOne이나
+    
+    
+    
+    
 }
--- Final-JPA-Shop/src/main/java/com/kh/springdb/repository/ProductRepository.java
+++ Final-JPA-Shop/src/main/java/com/kh/springdb/repository/ProductRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kh.springdb.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-	
+
 	//상세보기나 수정하기를 위한 메서드
 	Product findProductById(int id);
-	
-	//페이지네이션 처리 메서드
+	//오후에 페이지네이션 처리
 	Page<Product> findAll(Pageable pageable);
-	
+//
+ Page<Product> findAllByOrderByCreateDateDesc(Pageable pageable);
 }
